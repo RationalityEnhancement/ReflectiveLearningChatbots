1c0958489f4f2fe91c064004aeebad4a9f6d9ba7
srinidhisrinivas <srinidhisrinivas@hotmail.com>
2022-05-06 20:48:38 +0200

    added script to delete sensitive data

--- src/deleteSensitiveData.js
+++ src/deleteSensitiveData.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const readline = require('readline');
 const mongo = require('mongoose');
-const fs = require('fs');
-const path = require('node:path');
 const idMaps = require('./apiControllers/idMapApiController');
 const config = require('../json/config.json');
 
@@ -12,43 +10,44 @@ const DISCLAIMER_STRING = "I understand and I wish to continue";
 //--- database setup ---
 //----------------------
 
-(async () => {
+let deleteSensitiveData = async () => {
 
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    console.log('\x1b[41m\x1b[30m%s\x1b[0m', 'WARNING!');
-    console.log('Run this script only when your experiment is no longer running!')
-    console.log('If you delete this information, the chatbot will have to be restarted on every device that it is ' +
-        'currently running on.')
-    console.log('In order to continue with this step, type out \"' + DISCLAIMER_STRING + '\" (without quotes)' +
-        ' and press enter. If you type anything else, deletion will be aborted.')
-
-    rl.question("Type in the statement below, exactly as presented: ", (typed) => {
-        if(typed === DISCLAIMER_STRING){
-            console.log("Deletion in progress");
+    await mongo.connect(process.env.DB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true}, err => {
+        if (err) {
+            console.log(err);
         } else {
-            console.log("Deletion aborted.")
+            console.log('\x1b[42m\x1b[30m%s\x1b[0m', `Connected to the database`);
         }
-    })
-
-    rl.on('close', function () {
-        console.log('\nBYE BYE !!!');
-        process.exit(0);
     });
-    //
-    // await mongo.connect(process.env.DB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true}, err => {
-    //     if (err) {
-    //         console.log(err);
-    //     } else {
-    //         console.log('\x1b[42m\x1b[30m%s\x1b[0m', `Connected to the database`);
-    //     }
-    // });
+    console.log("\nDeleting Sensitive Data for Experiment with ID: " + config.experimentId);
+    await idMaps.remove(config.experimentId);
+    console.log("\nDeletion Complete\n")
+    await mongo.connection.close();
+};
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
+console.log('\n')
+console.log('\x1b[41m\x1b[37m%s\x1b[0m', 'WARNING!');
+console.log('This script deletes the sensitive identifying information of participants\' Telegram accounts. This ' +
+    'data is essential to the continued functioning of the experiment')
+console.log('Run this script only when your experiment is no longer running!')
+console.log('If you delete this information, the chatbot will have to be restarted on every device that it is ' +
+    'currently running on. Deletion cannot be undone!')
+console.log('In order to continue with this step, type out the following highlighted statement' +
+    ' and press enter. If you type anything else, deletion will be aborted.\n')
+console.log('\x1b[43m\x1b[30m%s\x1b[0m', DISCLAIMER_STRING)
 
-    // await mongo.connection.close();
-
-})();
+rl.question("\nType in the statement below, exactly as presented: \n", (typed) => {
+    typed = typed.trim();
+    if(typed === DISCLAIMER_STRING){
+        console.log("\nDeletion beginning");
+        deleteSensitiveData();
+    } else {
+        console.log("Deletion aborted.\n")
+    }
+    rl.close();
+})

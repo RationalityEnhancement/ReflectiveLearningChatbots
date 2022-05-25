89fa516bb614cd631d7c264685b9215a60a8f18a
srinidhisrinivas <srinidhisrinivas@hotmail.com>
2022-05-25 15:58:55 +0200

    tested ending and starting stages

--- src/stageHandler.js
+++ src/stageHandler.js
@@ -1,6 +1,9 @@
 const ReturnMethods = require('./returnMethods');
 const DevConfig = require('../json/devConfig.json');
 const lodash = require('lodash');
+const participants = require('./apiControllers/participantApiController')
+const {next} = require("lodash/seq");
+const moment = require('moment-timezone')
 
 /**
  * Stage handler class that takes in a config as a parameter
@@ -83,6 +86,8 @@ module.exports.getStageLengthDays = (config, conditionName, stageName) => {
  * field of the config file will directly be a list, instead of an
  * object with condition names
  *
+ * If no next stage is found, return partial failure with successdata -1
+ *
  * @param config
  * @param conditionName
  * @param stageName
@@ -138,3 +143,158 @@ module.exports.getNextStageName = (config, conditionName, stageName) => {
     }
 }
 
+module.exports.updateStageDay = async (config, uniqueId) => {
+    let newStageDay, participant;
+    try{
+        participant = await participants.get(uniqueId);
+    } catch(err){
+        return ReturnMethods.returnFailure("StageHandler: Unable to fetch participant to update stage day")
+    }
+
+    let currentStage, stageDay;
+    try{
+        currentStage = participant.stages.stageName;
+        stageDay = participant.stages.stageDay;
+    } catch(err){
+        return ReturnMethods.returnFailure("StageHandler: Stages object not found in participant");
+    }
+
+    if(typeof currentStage === "undefined" || typeof stageDay === "undefined"){
+        return ReturnMethods.returnPartialFailure("StageHandler: no stage has begun yet!", -1);
+    }
+
+    // Check if current stage has a specific length
+    let stageLengthObj = this.getStageLengthDays(config, participant.conditionName, currentStage);
+    if(stageLengthObj.returnCode === DevConfig.FAILURE_CODE){
+        return stageLengthObj;
+    }
+    let curStageLength = stageLengthObj.data;
+    newStageDay = stageDay + 1;
+
+    // Switch to next stage if number of days of current stage has been exceeded
+    if(curStageLength !== -1 && newStageDay > curStageLength){
+        let nextStageObj = this.getNextStageName(config, participant.conditionName, currentStage);
+        if(nextStageObj.returnCode === DevConfig.FAILURE_CODE){
+            return nextStageObj;
+        } else if(nextStageObj.returnCode === DevConfig.PARTIAL_FAILURE_CODE){
+            // End of experiment reached when no next stage found
+            newStageDay = -1;
+
+            // End the current stage
+            let endObj = await this.endCurrentStage(participant);
+            if(endObj.returnCode === DevConfig.FAILURE_CODE){
+                return endObj;
+            }
+
+            // End experiment
+            await participants.updateField(participant.uniqueId, "currentState", "experimentEnd");
+
+        } else {
+            // Start the next stage (which includes ending the current stage)
+            let startObj = await this.startStage(participant, nextStageObj.data);
+            if(startObj.returnCode === DevConfig.FAILURE_CODE){
+                return startObj;
+            }
+        }
+
+    } else {
+        // Otherwise update and move on
+        await participants.updateStageParameter(uniqueId, "stageDay", newStageDay);
+    }
+
+    return ReturnMethods.returnSuccess(newStageDay);
+}
+
+/**
+ *
+ * Ends the current stage for a given participant.
+ * This involves clearing certain stage parameters and updating the
+ * corresponding stage activity
+ *
+ * @param participant participant object must contain stage object
+ * @returns {Promise<{returnCode: *, data: *}>}
+ *          When failure, error code
+ *          When success, success code with no data
+ */
+module.exports.endCurrentStage = async (participant) => {
+
+    // get the current stage
+    let currentStage, stageDay;
+    try{
+        currentStage = participant.stages.stageName;
+        stageDay = participant.stages.stageDay;
+    } catch(err){
+        return ReturnMethods.returnFailure("StageHandler: Stages object not found in participant");
+    }
+
+    // If there is no current stage, then it cannot be ended
+    if(typeof currentStage === "undefined" || typeof stageDay === "undefined"){
+        return ReturnMethods.returnFailure("StageHandler: no stage has begun yet!", -1);
+    }
+
+    // Update the activity and the parameters
+    let now = moment.tz(participant.parameters.timezone);
+    try{
+        await participants.addStageActivity(participant.uniqueId, {
+            name : currentStage,
+            what : DevConfig.END_STAGE_STRING,
+            when : now.format()
+        })
+
+        // Erase current stage parameters
+        await participants.clearStageParam(participant.uniqueId, "stageName");
+        await participants.clearStageParam(participant.uniqueId, "stageDay");
+
+    } catch(err){
+        return ReturnMethods.returnFailure("StageHandler: Unable to update participant to end stage");
+    }
+    return ReturnMethods.returnSuccess("");
+}
+
+/**
+ *
+ * Starts a given stage for a given participant
+ *
+ * This involves setting certain stage variables and updating the activity
+ * of beginning and ending of stages
+ *
+ * @param participant participant object, must contain stages object
+ * @param nextStageName
+ * @returns {Promise<{returnCode: *, data: *}|*>}
+ *          When failure, error code
+ *          When success, success code with name of next stage
+ */
+module.exports.startStage = async (participant, nextStageName) => {
+
+    // Get the current stage and day
+    let currentStage, stageDay;
+    try{
+        currentStage = participant.stages.stageName;
+        stageDay = participant.stages.stageDay;
+    } catch(err){
+        return ReturnMethods.returnFailure("StageHandler: Stages object not found in participant");
+    }
+
+    // If a stage is already running, end it
+    if(typeof currentStage !== "undefined" || typeof stageDay !== "undefined"){
+        let endStageObj = await this.endCurrentStage(participant);
+        if(endStageObj.returnCode === DevConfig.FAILURE_CODE){
+            return endStageObj;
+        }
+    }
+
+    // Start the next stage
+    let now = moment.tz(participant.parameters.timezone);
+    try{
+        await participants.addStageActivity(participant.uniqueId, {
+            name : nextStageName,
+            what : DevConfig.BEGIN_STAGE_STRING,
+            when : now.format()
+        })
+        await participants.updateStageParameter(participant.uniqueId, "stageName", nextStageName);
+        await participants.updateStageParameter(participant.uniqueId, "stageDay", 0);
+    } catch(err){
+        return ReturnMethods.returnFailure("StageHandler: Unable to update participant parameters to start stage")
+    }
+    return ReturnMethods.returnSuccess(nextStageName);
+}
--- test/stageHandler.test.js
+++ test/stageHandler.test.js
@@ -4,19 +4,22 @@ const testConfigConds = require('../json/test/stageHandlerTestConfigConds.json')
 const DevConfig = require('../json/devConfig.json');
 
 const StageHandler = require('../src/StageHandler');
-
+const {MongoMemoryServer} = require("mongodb-memory-server");
+const mongo = require("mongoose");
+const participants = require("../src/apiControllers/participantApiController");
+const config = require("../json/config.json");
 
 describe('Get length days', () => {
     describe('No conditions', () => {
         it('Should return correct number of days', () => {
 
-            let returnObj = StageHandler.getStageLengthDays(testConfig, undefined, "Post")
+            let returnObj = StageHandler.getStageLengthDays(testConfig, undefined, "Post-Test")
             expect(returnObj.returnCode).to.equal(DevConfig.SUCCESS_CODE);
-            expect(returnObj.data).to.equal(testConfig.experimentStages[1].lengthDays)
+            expect(returnObj.data).to.equal(testConfig.experimentStages[2].lengthDays)
         })
         it('Should return -1', () => {
 
-            let returnObj = StageHandler.getStageLengthDays(testConfig, undefined, "Pre")
+            let returnObj = StageHandler.getStageLengthDays(testConfig, undefined, "Pre-Test")
             expect(returnObj.returnCode).to.equal(DevConfig.SUCCESS_CODE);
             expect(returnObj.data).to.equal(-1)
         })
@@ -76,14 +79,13 @@ describe('Get next stage name', () => {
     describe('No conditions', () => {
         it('Should return next stage name', () => {
 
-            let returnObj = StageHandler.getNextStageName(testConfig, undefined, "Pre")
-            console.log(returnObj)
+            let returnObj = StageHandler.getNextStageName(testConfig, undefined, "Pre-Test")
             expect(returnObj.returnCode).to.equal(DevConfig.SUCCESS_CODE);
             expect(returnObj.data).to.equal(testConfig.experimentStages[1].name)
         })
         it('Should return -1 when no next stage', () => {
 
-            let returnObj = StageHandler.getNextStageName(testConfig, undefined, "Post")
+            let returnObj = StageHandler.getNextStageName(testConfig, undefined, "Post-Test")
             expect(returnObj.returnCode).to.equal(DevConfig.PARTIAL_FAILURE_CODE);
             expect(returnObj.successData).to.equal(-1)
         })
@@ -127,6 +129,171 @@ describe('Get next stage name', () => {
         })
         it('Should fail when stage name is not a string', () => {
             let returnObj = StageHandler.getNextStageName(testConfigConds, "Experimental", true)
+            expect(returnObj.returnCode).to.equal(DevConfig.FAILURE_CODE);
+            console.log(returnObj.data);
+        })
+    })
+})
+
+const testPartId = "12345";
+
+describe('DB Connection', () => {
+
+    describe('Connecting to DB', () => {
+        it('Should connect to memory server', async () => {
+            let mongoServer = await MongoMemoryServer.create()
+            try {
+                await mongo.connect(mongoServer.getUri(), {dbName: "verifyMASTER"});
+                console.log('\tConnection successful!');
+            } catch (err) {
+                console.log(err);
+            }
+
+            const result = mongo.connection.readyState;
+            expect(result).to.equal(1);
+        });
+        it('Should add and update participant parameter', async () => {
+
+            await participants.add(testPartId);
+            await participants.initializeParticipant(testPartId, config)
+            let participant = await participants.get(testPartId);
+            expect(participant).to.not.be.null;
+            expect(participant.uniqueId).to.equal(testPartId);
+            expect(participant.parameters.language).to.equal("English");
+            assert("parameterTypes" in participant);
+            assert("stages" in participant);
+            assert("activity" in participant.stages);
+            assert("stageName" in participant.stages);
+            assert("stageDay" in participant.stages);
+        });
+    })
+})
+
+describe('End/Begin Stage', () => {
+    describe('No current stage - End', () => {
+        let returnObj;
+        it('Should return failure', async () => {
+            let participant = await participants.get(testPartId);
+            returnObj = await StageHandler.endCurrentStage(participant);
+            expect(returnObj.returnCode).to.equal(DevConfig.FAILURE_CODE);
+        })
+    })
+    describe('No current stage - begin', () => {
+        let returnObj, newPart;
+        let stageName = "Pre-Test";
+        it('Should return success', async () => {
+            let participant = await participants.get(testPartId);
+            returnObj = await StageHandler.startStage(participant, stageName);
+            expect(returnObj.returnCode).to.equal(DevConfig.SUCCESS_CODE);
+        })
+        it('Should have added start to stage activity', async () => {
+            newPart = await participants.get(testPartId);
+            expect(newPart.stages.activity.length).to.equal(1);
+            let newActivity = newPart.stages.activity[newPart.stages.activity.length-1];
+            expect(newActivity.name).to.equal(stageName);
+            expect(newActivity.what).to.equal(DevConfig.BEGIN_STAGE_STRING);
+            expect(typeof newActivity.when).to.equal("string");
+        })
+        it('Should have updated stage day and stage name', async () => {
+            expect(newPart.stages.stageDay).to.equal(0);
+            expect(newPart.stages.stageName).to.equal(stageName);
+        })
+    })
+
+    describe('Yes current stage - end', () => {
+        let returnObj, newPart;
+        let stageName = "Pre-Test";
+        it('Should return success', async () => {
+            let participant = await participants.get(testPartId);
+            returnObj = await StageHandler.endCurrentStage(participant, stageName);
+            expect(returnObj.returnCode).to.equal(DevConfig.SUCCESS_CODE);
+        })
+        it('Should have added end to stage activity', async () => {
+            newPart = await participants.get(testPartId);
+            expect(newPart.stages.activity.length).to.equal(2);
+            let newActivity = newPart.stages.activity[newPart.stages.activity.length-1];
+            expect(newActivity.name).to.equal(stageName);
+            expect(newActivity.what).to.equal(DevConfig.END_STAGE_STRING);
+            expect(typeof newActivity.when).to.equal("string");
+        })
+        it('Should have erased stage day and stage name', async () => {
+            expect(newPart.stages.stageDay).to.be.undefined;
+            expect(newPart.stages.stageName).to.be.undefined;
+        })
+    })
+
+    describe('No current stage - begin 2', () => {
+        let returnObj, newPart;
+        let stageName = "Test";
+        it('Should return success', async () => {
+            let participant = await participants.get(testPartId);
+            returnObj = await StageHandler.startStage(participant, stageName);
+            expect(returnObj.returnCode).to.equal(DevConfig.SUCCESS_CODE);
+        })
+        it('Should have added start to stage activity', async () => {
+            newPart = await participants.get(testPartId);
+            expect(newPart.stages.activity.length).to.equal(3);
+            let newActivity = newPart.stages.activity[newPart.stages.activity.length-1];
+            expect(newActivity.name).to.equal(stageName);
+            expect(newActivity.what).to.equal(DevConfig.BEGIN_STAGE_STRING);
+            expect(typeof newActivity.when).to.equal("string");
+        })
+        it('Should have updated stage day and stage name', async () => {
+            expect(newPart.stages.stageDay).to.equal(0);
+            expect(newPart.stages.stageName).to.equal(stageName);
+        })
+    })
+
+    describe('Yes current stage - begin', () => {
+        let returnObj, newPart;
+        let stageName = "Post-Test";
+        it('Should return success', async () => {
+            let participant = await participants.get(testPartId);
+            returnObj = await StageHandler.startStage(participant, stageName);
+            expect(returnObj.returnCode).to.equal(DevConfig.SUCCESS_CODE);
+        })
+        it('Should have added end of last stage to stage activity', async () => {
+            newPart = await participants.get(testPartId);
+            expect(newPart.stages.activity.length).to.equal(5);
+            let newActivity = newPart.stages.activity[newPart.stages.activity.length-2];
+            expect(newActivity.name).to.equal("Test");
+            expect(newActivity.what).to.equal(DevConfig.END_STAGE_STRING);
+            expect(typeof newActivity.when).to.equal("string");
+        })
+        it('Should have added start to stage activity', async () => {
+            newPart = await participants.get(testPartId);
+            expect(newPart.stages.activity.length).to.equal(5);
+            let newActivity = newPart.stages.activity[newPart.stages.activity.length-1];
+            expect(newActivity.name).to.equal(stageName);
+            expect(newActivity.what).to.equal(DevConfig.BEGIN_STAGE_STRING);
+            expect(typeof newActivity.when).to.equal("string");
+        })
+        it('Should have updated stage day and stage name', async () => {
+            expect(newPart.stages.stageDay).to.equal(0);
+            expect(newPart.stages.stageName).to.equal(stageName);
+        })
+    })
+
+    describe('Fail when stages not present - begin', () => {
+        let returnObj, newPart;
+        let stageName = "Test";
+        it('Should return success', async () => {
+            let participant = await participants.get(testPartId);
+            let copyPart = JSON.parse(JSON.stringify(participant));
+            delete copyPart["stages"]
+            returnObj = await StageHandler.startStage(copyPart, stageName);
+            expect(returnObj.returnCode).to.equal(DevConfig.FAILURE_CODE);
+            console.log(returnObj.data);
+        })
+    })
+    describe('Fail when stages not present - end', () => {
+        let returnObj, newPart;
+        let stageName = "Test";
+        it('Should return success', async () => {
+            let participant = await participants.get(testPartId);
+            let copyPart = JSON.parse(JSON.stringify(participant));
+            delete copyPart["stages"]
+            returnObj = await StageHandler.endCurrentStage(copyPart);
             expect(returnObj.returnCode).to.equal(DevConfig.FAILURE_CODE);
             console.log(returnObj.data);
         })

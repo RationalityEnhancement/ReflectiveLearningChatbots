6115f8246195cd57ed119fe9138572595b91d6c8
srinidhisrinivas <srinidhisrinivas@hotmail.com>
2022-07-06 09:29:18 +0200

    added documentation for buttonlayout

--- src/inputOptions.js
+++ src/inputOptions.js
@@ -1,14 +1,32 @@
-const { Telegraf, Markup } = require('telegraf');
+const { Markup } = require('telegraf');
 
 const ConfigReader = require('./configReader');
-const {copy} = require("request/lib/helpers");
 const config = ConfigReader.getExpConfig();
 
+/**
+ *
+ * Get the telegram button layout based on the options to choose
+ * from and the number of columns the options should be evenly
+ * distributed over.
+ *
+ * With multiChoice questions (when terminateWord) is passed,
+ * the Done button always occurs on its own in the last row
+ *
+ * @param options Options to go on buttons
+ * @param buttonLayoutCols number of columns to distribute over. Default is 1
+ * @param terminateWord For multiChoice questions - text to terminate choice
+ * @returns {*[]}
+ */
 module.exports.getButtonLayout = (options, buttonLayoutCols, terminateWord) => {
+	// Validate inputs
 	if(typeof buttonLayoutCols !== "number") buttonLayoutCols = undefined;
 	buttonLayoutCols = Math.ceil(buttonLayoutCols);
 	if(buttonLayoutCols < 1) buttonLayoutCols = undefined;
 	if(buttonLayoutCols > options.length) buttonLayoutCols = options.length;
+
+	// Either string or undefined
+	if(typeof terminateWord !== "string") terminateWord = undefined;
+
 	let updatedOptions;
 	if(buttonLayoutCols) {
 		updatedOptions = []
